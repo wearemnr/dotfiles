e74077b6a8eae2df4add379a9be591416f61315c
wearemnr <wearemnr@pm.me>
2021-05-25 18:44:16 +0300

    Small changes

--- system/modules/fonts.nix
+++ system/modules/fonts.nix
@@ -3,9 +3,12 @@
 {
   fonts = {
     fonts = with pkgs; [
+      wamnr.iosevka-ss08
+      wamnr.iosevka-term-ss08
+      wamnr.iosevka-fixed-ss08
       fira-code
       fantasque-sans-mono
-      iosevka-term-ss08-font
+      # iosevka-term-ss08-font
       mononoki
       unifont
     ];
